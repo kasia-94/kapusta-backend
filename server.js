ba5053d968b74c56bd19b9e3393272f50b205b96
Kateryna Berehovych <kasia-94@ukr.net>
2023-03-07 20:09:07 +0200

    Fix

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ dotenv.config();
 
 mongoose.set("strictQuery", false);
 
-const { HOST_URI } = process.env;
-
 async function main() {
   try {
-    await mongoose.connect(HOST_URI);
+    await mongoose.connect(process.env.HOST_URI);
     console.log("Database connection successful");
     app.listen(3000, () => {
       console.log("Server running. Use our API on port: 3000");
-      console.log("Docs available at http://localhost:3000/api-docs/")
+      console.log("Docs available at http://localhost:3000/api-docs/");
     });
   } catch (error) {
     console.error("Main failed:", error.message);
